0c4fbb17ff050cbe6b7dc5392b51081c41cee5e9
Clint Parker <clintcparker@gmail.com>
2022-04-12 11:57:05 -0700

    cleaning up the scripts

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 if ! command -v brew &> /dev/null
 then
     echo "homebrew could not be found, installing"
@@ -7,12 +9,17 @@ fi
 brew install rcm
 brew install asdf
 
-
+if [ ! -d "$HOME/.dotfiles" ]; then
+    git clone https://github.com/clintcparker/dotfiles.git $HOME/.dotfiles
+    git -C $HOME/.dotfiles remote set-url --push origin git@github.com:clintcparker/dotfiles.git
+fi
+chmod +x $HOME/.dotfiles/hooks/post-up
 
 for tool in `cat tool-versions | awk '{ print $1 }'`; do
     echo "adding plugin $tool"
     asdf plugin add $tool
 done
+
 
 rcup
 
@@ -23,3 +30,8 @@ gem install bundler
 bundle install
 
 chsh -s `which fish`
+
+asdf direnv setup --shell fish --version latest
+
+fish -c "fish_add_path ~/.bin"
+
