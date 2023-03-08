28649fedf7fc01c884c2674328fd6e303a7f2e1e
Clint Parker <clintcparker@gmail.com>
2023-03-08 14:58:57 +0000

    just install brew, fish, rcm

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# Install Homebrew
 if ! command -v brew &> /dev/null
 then
     echo "homebrew could not be found, installing"
@@ -8,31 +9,32 @@ fi
 
 # if /home/linuxbrew/.linuxbrew/bin/brew exists, then we are on linux
 if [ -f "/home/linuxbrew/.linuxbrew/bin/brew" ]; then
-    echo "linux detected, installing linuxbrew"
+    echo "linux detected, configuring linuxbrew"
     (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> $HOME/.profile
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-    sudo apt-get install build-essential
+    sudo apt-get install build-essential --fix-missing
 fi
 
-brew install rcm
-brew install asdf
+echo "Homebrew installed!"
+
+# Install fish
 brew install fish
+chsh -s `which fish`
+fish -c "fish_add_path ~/.bin"
+fish -c "fish_add_path ~/.dotnet/tools"
+
+echo "fish installed!"
+
+# Install rcm
+brew install rcm
 
 if [ ! -d "$HOME/.dotfiles" ]; then
-    git clone https://github.com/clintcparker/dotfiles.git $HOME/.dotfiles
+    git clone -b 3-7-23 https://github.com/clintcparker/dotfiles.git $HOME/.dotfiles
     git -C $HOME/.dotfiles remote set-url --push origin git@github.com:clintcparker/dotfiles.git
 fi
 chmod +x $HOME/.dotfiles/hooks/post-up
 
-cd $HOME/.dotfiles
-
-for tool in `cat tool-versions | awk '{ print $1 }'`; do
-    echo "adding plugin $tool"
-    asdf plugin-add $tool
-    asdf plugin-update $tool
-done
-
-
+echo "rcm installed!"
 
 cd $HOME
 echo "now in:"
@@ -40,29 +42,10 @@ pwd
 rcup
 
 
-echo "brew bundle --global"
-brew bundle --global
 
 
-echo "brew bundle --global"
-brew bundle --global
 
-
-asdf install
-
-chsh -s `which fish`
-
-asdf direnv setup --shell fish --version latest
-
-./$HOME/.asdf/shims/gem install bundler
-
-./$HOME/.asdf/shims/bundle install
-
-
-fish -c "fish_add_path ~/.bin"
-
-fish -c "fish_add_path ~/.dotnet/tools"
 
 echo "Done!"
-echo "type 'fish' to start using fish"
+#  echo "type 'fish' to start using fish"
 fish
